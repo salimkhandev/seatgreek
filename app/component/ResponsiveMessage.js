fe7982e7cf4d878b802ca960168cd4685314d751
salimkhandev <salimt450s@gmail.com>
2024-11-30 00:36:19 +0500

    improved

--- app/component/ResponsiveMessage.js
+++ app/component/ResponsiveMessage.js
@@ -23,7 +23,7 @@ const ResponsiveMessage = () => {
     return (
         <div className="message-box">
             <h1>Oops! This site is best viewed on a Laptop</h1>
-            <p>It seems like you're using a device other than a laptop. For the best experience, please visit this website on a laptop or desktop computer.</p>
+            <p>It seems like you&apos;re using a device other than a laptop. For the best experience, please visit this website on a laptop or desktop computer.</p>
             <p><a href="https://www.example.com" target="_blank" rel="noopener noreferrer">Click here for more information</a></p>
         </div>
     );
