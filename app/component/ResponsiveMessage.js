2828773814ae583ad7d5124d03e28bfa253b7039
salimkhandev <salimt450s@gmail.com>
2024-11-30 00:57:08 +0500

    improved

--- app/component/ResponsiveMessage.js
+++ app/component/ResponsiveMessage.js
@@ -10,7 +10,9 @@ const ResponsiveMessage = () => {
     return (
         <div className="message-box">
             <h1>Oops! This site is best viewed on a Laptop</h1>
-            <p>It seems like you&apos;re using a device other than a laptop , mobile version is comming soon. For the best experience, please visit this website on a laptop or desktop computer.</p>
+            <p>It seems like you&apos;re using a device other than a laptop . For the best experience, please visit this website on a laptop or desktop computer. 
+                <em>Mobile version is comming soon!</em>
+            </p>
             <p><a href="https://salimkhandev.vercel.app" target="_blank" rel="noopener noreferrer">Click here for more information</a></p>
         </div>
     );
